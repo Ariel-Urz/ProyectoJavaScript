02ebf9256e4760c555ffa47447174beec8c91984
Ariel Urzagasti <arielurzagasti9@gmail.com>
2022-05-19 22:43:50 -0300

    Segunda Entrega De Proyecto

--- index.js
+++ index.js
@@ -1,212 +1,117 @@
+const contenedorProductos = document.getElementById('contenedor-productos')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
 
-function registro(){
+let carrito = []
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
 
-    let usuario = prompt("Ingrese Usario");
-    let contraseña = prompt("Ingrese contraseña");
-    let confirContraseña = prompt("Repetir contraseña");
-    
-    /*Si la contraseñas no coinciden vuelve a repetir*/
-    
-    while (contraseña != confirContraseña){
-    
-        alert("su contraseña esta mal");
-        contraseña = prompt("Ingrese contraseña ");
-        confirContraseña = prompt("Repetir contraseña ");
-    
-        if (contraseña == confirContraseña){
-           alert("las contraseñas coiciden")
-        }
-    
-    }
-    
-    alert("Registro Completo \n" + "Usuario: "+ usuario + "\n" + "contraseña: " + contraseña);
-    
-    
-    }
-    function compraUno(){
-    
-        let juguete = prompt("Elija entre 3 produnctos con stock: \n" +
-    
-                            "1) Juguete de Accion \n" +
-                            "2) Juguete de Mesa \n" +
-                            "3) Juguete de Bebe")
-      
-        switch (juguete) {
-    
-            /*Opcion 1 Juguete de Accion*/
-            case "1":
-    
-                alert("Seleccionaste Juguete de accion \n"+
-                      "El precio es 50$ por unidad");   
-                precio = 50;
-                cantidad = parseInt(prompt("Ingrese la cantidad que desea comprar"));
-                resultado = precio * cantidad;
-                alert("cantidad de juguetes seleccionado "+ cantidad + "\nEl monto total a pagar es " + resultado + "$");
-                break;
-    
-            /*Opcion 2 Juguete de Mesa*/
-    
-            case "2":
-    
-                alert("Seleccionaste Juguete de Mesa \n"+
-                      "El precio es 100$ por unidad");   
-                precio = 100;
-                cantidad = parseInt(prompt("Ingrese la cantidad que desea comprar"));
-                resultado = precio * cantidad;
-                alert("cantidad de juguetes seleccionado "+ cantidad + "\nEl monto total a pagar es " + resultado + "$");
-                break;
-    
-            /*Opcion 3 Juguete de Bebe*/
-    
-            case "3":
-    
-                alert("Seleccionaste Juguete de Bebe \n"+
-                      "El precio es 25$ por unidad");   
-                precio = 25;
-                cantidad = parseInt(prompt("Ingrese la cantidad que desea comprar"));
-                resultado = precio * cantidad;
-                alert("cantidad de juguetes seleccionado "+ cantidad + "\nEl monto total a pagar es " + resultado + "$");
-                break;
-    
-            default:
-                alert("El valor seleccionado no tiene stock");
-                break;
-    
-        }
-    
-    
-      
-    }
-    
-    class Sugerencia{
-    
-        constructor (marca,nombre){
-    
-                this.marca = marca;
-                this.nombre = nombre;
-        }
-    
-        mostrar(){
-                alert("Marca Sugerida " + this.marca + "\n" + "Juguete Sugerido " + this.nombre);
-        }
-    
-    }
-    
-    
-    // Ejecucion del codigo
-    
-    //Inicio del Registro
-    registro();
-    
-    //Inicio de Compra
-    
-    compraUno()
-    
-    seguir = prompt("Si desea continuar comprando escriba (si): ");
-    
-    while (seguir == "si"){
-    
-            compraUno();
-    
-            seguir = prompt("Desea Continuar Comprando");
-    }
-    
-    alert("Compra finalizada");
-    
-    //Sugerencia para el catalogo
-    
-    const catalogo = ["IronMan","SpiderMan","Hulk","Thor","Peluche","Autos","Princesa Disney","Bebes"];
-    
-    alert("Catalogo para el proximo mes\n" + catalogo.join("\n"));
-    
-    alert("Por favor sugiera Productos para agregar a nuestro catalogo");
-    
-    opcion = "si"
-    
-    while (opcion == "si"){
-    
-        let marcaNueva = prompt("Sugiera alguna marca de Juguete: ");
-        let jugueteNuevo = prompt("Suguiera algun Juguete: ");
-        const producto = new Sugerencia(marcaNueva, jugueteNuevo);
-        producto.mostrar();
-        catalogo.push(producto.nombre);
-        alert("Sus suguerencias seran evaluadas para agragar al siguiente catalogo " + catalogo.join("\n"));
-        opcion = prompt("Si Desea Sugerir otro prducoto escriba (si): ");
-        }
-        alert(catalogo.join("\n"))
-    
-    let nuevoCatalogo=catalogo
-    
-    //Sugerencia para borrar producto del catalogo
-    
-    aceptar = prompt("deseas sugerir eliminar algo del catalogo escribe (si): ");
-    
-    while (aceptar == "si"){
-    
-        elejido = prompt("este es nuestro catalogo, que desea elminar?\n" + nuevoCatalogo.join("\n"));
-    
-        let index = nuevoCatalogo.indexOf(elejido);
-    
-        //Elimina el producto si esta en el catalogo
-    
-        if (index != -1){
-    
-            nuevoCatalogo.splice(index , 1);
-    
-            alert(nuevoCatalogo.join("\n"));
-    
-            aceptar = prompt("desea eliminar algo mas? escriba (si): ");
-        }
-    
-        //Si no esta el producto en nuestro catalogo
-    
-        else{
-            alert("el nombre sugerido no esta en nuestro catalgo");
-    
-            aceptar = prompt("deseas volver a intentar escriba(si): ");
-        }  
-    }
-    
-    alert("Gracias por las sugerencias")
-    
-    //busqueda en el catalogo
-    aceptBusqueda = prompt("deseas encontrar algun producto en nuestro catalogo? escriba(si)");
-    
-    while ( aceptBusqueda == "si"){
-    
-        let busquedaJuguete = prompt("Que producto desea encontrar?: ");
-    
-        const busqueda = nuevoCatalogo.filter(elem => elem === busquedaJuguete);
-    
-        //Si encuentra muestra el producto
-    
-        if (busqueda == busquedaJuguete){
-    
-            alert("Pruducto encontrado en nuestro Catalogo " + busqueda);
-            aceptBusqueda = prompt("desea buscar otro producto?"); 
-        }
-    
-        //Sino encuentra el producto
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+//Crea productos en el html
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt= "">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p>marca: ${producto.marca}</p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+    `
+    contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById(`agregar${producto.id}`)
+    //Por cada elemento de mi array, creo un div, lo cuelgo, le pongo un id particular, una vez colgado
+    //le hago un get element by id (el de agregar) Obtengo el elemento y a dicho elemento le agregamos
+    //el add event listener
+
+    boton.addEventListener('click', () => {
+        //esta funcion ejecuta el agregar el carrito con la id del producto
+        agregarAlCarrito(producto.id)
         
-        else{
-            alert("Producto no encontrado");
-            aceptBusqueda = prompt("desea buscar otro producto?");
-        }
+    })
+})
+
+
+
+//AGREGAR AL CARRITO
+const agregarAlCarrito = (prodId) => {
+
+    // AUMENTAR LA CANTIDAD 
+    const existe = carrito.some (prod => prod.id === prodId) 
+
+    //SI YA ESTÁ EN EL CARRITO, ACTUALIZAMOS LA CANTIDAD
+
+    if (existe){ 
+        const prod = carrito.map (prod => { 
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+
+        //EN CASO DE QUE NO ESTÉ, AGREGAMOS AL CARRITO
+    } else { 
+        const item = stockProductos.find((prod) => prod.id === prodId)
+        carrito.push(item)
     }
     
-    alert("Gracias Por Visitarnos Fin")
+    //MODIFICA EL CARRITO
+    actualizarCarrito() 
     
+}
 
 
+//ELIMINAR LOS PRODUCTOS DEL CARRITO
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
 
+    const indice = carrito.indexOf(item)
 
-
-
-
-
-
-
-
-
+    carrito.splice(indice, 1) 
+     
+    actualizarCarrito() 
     
+    console.log(carrito)
+}
+
+//AGREGAMOS AL MODAL LOS PRODUC Y SUMAMOS
+const actualizarCarrito = () => {
+    
+    contenedorCarrito.innerHTML = ""
+    
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+
+        contenedorCarrito.appendChild(div)
+        
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    })
+    
+    contadorCarrito.innerText = carrito.length 
+    
+    console.log(carrito)
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+    
+}
